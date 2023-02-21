f82cbf5b5345b9a624435598485446cee0716b17
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-02-21 13:49:51 +0100

    feat/add course index endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('/courses')
+    ->name('courses.')
+    ->controller(CourseController::class)
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+    });
