b35bca41793f63ab801cb2a56df750d8e4591355
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-02-21 16:38:36 +0100

    feat/add CourseFactory

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(),
+            'price' => $this->faker->randomFloat(2, 100, 300),
+            'sales' => $this->faker->numberBetween(0, 1000),
+        ];
+    }
+}
